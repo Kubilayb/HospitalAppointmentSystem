d5dca9b42699eec141d7ba174d193ee0578dcdaa
Kubilayb <109307078+Kubilayb@users.noreply.github.com>
2024-06-07 23:14:34 +0300

    Errors fixed. Folders organized.DB created.

---
+++ HospitalAppointmentSystemBackend/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+
+namespace Domain.Entities
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
+        public virtual ICollection<Feedback> Feedbacks { get; set; }
+    }
+}
